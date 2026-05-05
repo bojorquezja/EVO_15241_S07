fb4832264cb7ccb15dc8ac2d95398add08b27a7b
bojorquezja <bojorquez.ja@gmail.com>
2026-05-05 18:56:57 -0500

    Agregar archivos de proyecto.

---
+++ DIP/OrderProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP {
+    internal class OrderProcessor {
+        private readonly EmailNotification _emailNotification;
+        public OrderProcessor() {
+            _emailNotification = new EmailNotification();
+        }
+        public void ProcessOrder() {
+            // Order processing logic
+            Console.WriteLine("Order processed.");
+            // Send notification
+            _emailNotification.Send("Your order has been processed.");
+        }
+    }
+}
---
+++ DIP/Program.cs
@@ -0,0 +1,5 @@
+﻿// Use EmailNotification CASO BASE
+using DIP;
+
+var orderProcessor = new OrderProcessor();
+orderProcessor.ProcessOrder();
